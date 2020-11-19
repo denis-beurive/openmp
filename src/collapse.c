2b5a2827228c6b2ab42361717203d01ce1a7dbd4
denis-beurive <denis.beurive@gmail.com>
2020-11-19 20:11:33 +0100

    Add schedule

--- src/collapse.c
+++ src/collapse.c
@@ -6,12 +6,14 @@ int main() {
 
     int i = 0;
     int j = 0;
-    printf("i | j\n");
-    #pragma omp parallel for num_threads(4) default(none) shared(i) private(j)
+
+    printf("==========================================================\n");
+
+    printf("i | j   | thread\n");
+    #pragma omp parallel for num_threads(4) default(none) shared(i) private(j) schedule(static, 2)
     for (i = 0; i < 4; i++)
         for (j = 0; j <= 6; j++) {
-            printf("%d | %03d #%d\n", i, j, omp_get_thread_num());
-            usleep(1000);
+            printf("%d | %03d | #%d\n", i, j, omp_get_thread_num());
         }
 
     printf("==========================================================\n");
@@ -20,13 +22,10 @@ int main() {
     // Note: if execution of any associated loop changes any of the values used
     //       to compute any of the iteration counts, then the behavior is unspecified.
 
-    printf("i | j\n");
-    #pragma omp parallel for num_threads(4) default(none) shared(i) private(j) collapse(2)
+    printf("i | j   | thread\n");
+    #pragma omp parallel for num_threads(4) default(none) shared(i) private(j) collapse(2) schedule(static, 2)
     for (i = 0; i < 4; i++)
         for (j = 0; j <= 6; j++) {
-            printf("%d | %03d #%d\n", i, j, omp_get_thread_num());
-            usleep(1000);
+            printf("%d | %03d | #%d\n", i, j, omp_get_thread_num());
         }
-
-
 }
